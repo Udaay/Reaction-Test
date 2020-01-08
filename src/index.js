8c4ce5049de997e9a7818aba8a7978ca19fafef3
Uday <Uday>
2020-01-08 17:37:22 +0530

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -2,22 +2,30 @@ window.onload = function() {
 
 var start = new Date().getTime();
 var box = document.createElement("div");
-var details = document.createElement("div")
+var detail = document.getElementById("detail")
 
 function changeAttr() {
   let myBox = box.style;
   let left = Math.floor(Math.random() *700);
   let top = Math.floor(Math.random() *550 );
+  let width = Math.floor(Math.random() * (50)) + 50;
   console.info(left,top);
   myBox.left = `${left}px`;
   myBox.top = `${top}px`;
+  myBox.width = `${width}px`
+  myBox.height = myBox.width;
   myBox.backgroundColor = colorGenerator();
+  start = new Date().getTime();
   myBox.display ='block';
 }
 
 function colorGenerator() {
-  var letters = ['blue','red','yellow','red','green','red'];
-  var color = letters[Math.floor(Math.random() * letters.length )] ;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for(let i =1; i<=6;i++)
+  {
+    color += letters[Math.floor(Math.random() * letters.length )] ;
+  }
   return color;
 }
 
@@ -25,9 +33,6 @@ function createBox() {
   box.setAttribute('id','mybox');
   changeAttr();
   document.getElementById('root').appendChild(box);
-  details.setAttribute('id','detail')
-  
-  document.getElementById('root').appendChild(details)
   box.addEventListener('click',handleClick);
 }
 
@@ -35,8 +40,7 @@ function handleClick() {
   var end = new Date().getTime();
   var diff = (end - start)/1000;
   console.info(`Time taken ${diff} seconds`);
-  details.innerHTML = `Your response time ${diff} seconds `
-  start = new Date().getTime();
+  detail.innerHTML = `Your response time ${diff} seconds `
   box.style.display = 'none';
   setTimeout(changeAttr,1000);
 }
